e0ae91f5a34b1571b0a8b13e57dbeb5c22ba37b0
izumin5210 <m@izum.in>
2018-12-13 09:55:06 +0900

    Use NewContext and FromContext instead of nrutil's one

--- util.go
+++ util.go
@@ -8,13 +8,13 @@ import (
 
 type ctxKeyClientSegment struct{}
 
-func setSegment(ctx context.Context, seg newrelic.Segment) context.Context {
+func setSegment(ctx context.Context, seg *newrelic.Segment) context.Context {
 	return context.WithValue(ctx, ctxKeyClientSegment{}, seg)
 }
 
-func getSegment(ctx context.Context) (st newrelic.Segment, ok bool) {
+func getSegment(ctx context.Context) (st *newrelic.Segment, ok bool) {
 	if v := ctx.Value(ctxKeyClientSegment{}); v != nil {
-		st, ok = v.(newrelic.Segment)
+		st, ok = v.(*newrelic.Segment)
 	}
 	return
 }
